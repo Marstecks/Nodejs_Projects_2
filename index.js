a493158efb3ff1e18d755d47dbe9699b2c984f34
Marstecks <93575286+Marstecks@users.noreply.github.com>
2022-12-20 09:16:16 +0000

    Running our first program

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var PORT = process.abort.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello world from marvelous Innocent at GitHub codespace');
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening from Port ==> ${PORT}`);
+})
