8066d14f2dd3072e23200c2a61fb90996046e45e
Marstecks <93575286+Marstecks@users.noreply.github.com>
2022-12-29 06:51:07 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var PORT = process.abort.PORT || 8080;
-
+var message =
 app.get('/', (req, res) => {
-    res.send('Hello world from marvelous Innocent at GitHub codespace');
+    res.send('Hello world from marvelous Innocent at GitHub codespace.');
 
 });
 
