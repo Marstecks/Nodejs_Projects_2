11d93dc59f92617e0246c54c851b733e6ccb1fd0
Marstecks <93575286+Marstecks@users.noreply.github.com>
2022-12-29 06:56:31 +0100

    update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 var PORT = process.abort.PORT || 8080;
 
 var date = new Date(); 
-var message = {
+var msg = {
     greetings: "Hello world from marvelous Innocent at GitHub codespace. The Year is coming to an end in the next few days, and God has been faithful and forever will remain faithful",
-    date: `${date.getDay()} : ${date.getMonth()} : ${date.ge}`
+    date: `${date.getDay()} : ${date.getMonth()} : ${date.getYear()}`,
 }
 app.get('/', (req, res) => {
     res.json();
